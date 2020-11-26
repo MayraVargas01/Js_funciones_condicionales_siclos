6204c583bf2e8b1817064ff8c1187d71405f41dd
Marìa Carolina Gòmez <mayravargase01@gmail.com>
2020-11-25 21:08:39 -0500

    25-11-2020 Funciones

---
+++ main.js
@@ -0,0 +1,39 @@
+// // let = prompt("ingrese Edad");
+// // if(edad >= 18){
+// // alert("Usted es mayor de edad");
+// // }else{
+// //     alert("Usted es menor de edad");
+// }
+
+let tarjeta = true;
+if(tarjeta != true){
+alert("Ingreso por la puerta");
+}else{
+    alert("No puede ingresar");
+}
+
+
+let numero = 23;
+if(numero=== 23){
+alert("Numero corresponde");
+}else{
+    alert("Numero no corresponde");
+}
+
+
+let num = prompt("ingrese un numero mayor que (10)");
+if(num >= 10){
+alert("El numero ingresado es : "+num+" y es mayor o igual que 10");
+}else{
+    alert("Numero no corresponde");
+}
+
+
+function Mayra(plata){
+    let Leche=5000;
+    let Papas=1500;
+    let vueltos;
+    vueltos = plata-Leche;
+    vueltos -= Papas;
+    return vueltos;
+}
